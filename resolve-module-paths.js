21bca61cfa55361941d8a4123b16a54a97666027
Mariusz Nowak <medyk@medikoo.com>
2019-02-01 17:47:11 +0100

    refactor: cleanup logic

--- resolve-module-paths.js
+++ resolve-module-paths.js
@@ -1,27 +1,23 @@
 "use strict";
 
-const primitiveSet               = require("es5-ext/object/primitive-set")
-    , ensureString               = require("es5-ext/object/validate-stringifiable-value")
+const ensureString               = require("es5-ext/object/validate-stringifiable-value")
     , { basename, resolve, sep } = require("path")
     , readdir                    = require("fs2/readdir");
+
+const dirsBlackList = new Set([
+	"bin", "docs", "documentation", "examples", "node_modules", "test", "tests", "spec", "specs"
+]);
 
 const readdirOptions = {
 	depth: Infinity,
 	type: { file: true },
 	pattern: /\.js$/u,
 	ignoreRules: ["git"],
-	dirFilter: (function () {
-		const blackList = primitiveSet(
-			"bin", "docs", "documentation", "examples", "node_modules", "test", "tests", "spec",
-			"specs"
-		);
-
-		return path => {
-			if (blackList[basename(path)]) return false;
-			if (path.endsWith(`${ sep }lib${ sep }private`)) return false;
-			return true;
-		};
-	})()
+	dirFilter: path => {
+		if (dirsBlackList.has(basename(path))) return false;
+		if (path.endsWith(`${ sep }lib${ sep }private`)) return false;
+		return true;
+	}
 };
 
 module.exports = function (path) {
