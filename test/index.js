a1b1c21fe96d7257d0b281e130223c066e529913
Mariusz Nowak <medyk@medikoo.com>
2019-02-04 10:12:52 +0100

    feat: refactor into async/await

--- test/index.js
+++ test/index.js
@@ -1,27 +1,26 @@
 "use strict";
 
-const Deferred          = require("deferred")
-    , proxyquire        = require("proxyquire")
+const proxyquire        = require("proxyquire")
     , { resolve }       = require("path")
     , unlink            = require("fs2/unlink")
     , tape              = require("tape")
     , generateTestIndex = require("../");
 
-tape("Generate index", t => {
+tape("Generate index", async t => {
 	const playgroundPath = resolve(__dirname, "_playground");
 
-	generateTestIndex(playgroundPath)(() => {
-		const pgTest = tape.createHarness(), deferred = new Deferred();
-
-		pgTest.createStream().on("close", deferred.resolve);
+	await generateTestIndex(playgroundPath);
+	const pgTest = tape.createHarness();
+	await new Promise(resolve => {
+		pgTest.createStream().on("close", resolve);
 		proxyquire("./_playground/test.index", { tape: pgTest });
-		return deferred.promise(() => {
-			const results = pgTest._results;
+	});
+	const results = pgTest._results;
 
-			t.equal(results.fail, 1);
-			t.equal(results.pass, 2);
-			t.equal(global.testALoaded, true);
-			return unlink(resolve(playgroundPath, "test.index.js"));
-		});
-	}).done(t.end, t.end);
+	t.equal(results.fail, 1);
+	t.equal(results.pass, 2);
+	t.equal(global.testALoaded, true);
+	await unlink(resolve(playgroundPath, "test.index.js"));
+
+	t.end();
 });
