c2b24a6f05107988bd3f34acbeee40190ff590dc
Mariusz Nowak <medyk@medikoo.com>
2019-02-04 10:53:14 +0100

    feat: support multiple test files for single module

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
 "use strict";
 
-const ensureString       = require("es5-ext/object/validate-stringifiable-value")
-    , reEscape           = require("es5-ext/reg-exp/escape")
-    , { resolve, sep }   = require("path")
-    , lstat              = require("fs2/lstat")
-    , writeFile          = require("fs2/write-file")
-    , resolveModulePaths = require("./resolve-module-paths")
-    , testIndexName      = require("./lib/test-index-name");
+const ensureString                     = require("es5-ext/object/validate-stringifiable-value")
+    , reEscape                         = require("es5-ext/reg-exp/escape")
+    , { basename, resolve, sep, join } = require("path")
+    , lstat                            = require("fs2/lstat")
+    , readdir                          = require("fs2/readdir")
+    , writeFile                        = require("fs2/write-file")
+    , resolveModulePaths               = require("./resolve-module-paths")
+    , testIndexName                    = require("./lib/test-index-name");
 
 const { push } = Array.prototype, sepRe = new RegExp(reEscape(sep), "gu");
 
-module.exports = async function (path) {
-	path = resolve(ensureString(path));
-	const testPath = resolve(path, "test"), modulesTestsData = [], indexTestsData = [];
-	const modulesData = await Promise.all(
-		(await resolveModulePaths(path)).map(async modulePath => ({
-			modulePath,
-			stats: await lstat(resolve(testPath, modulePath), { loose: true })
-		}))
+module.exports = async function (packagePath) {
+	packagePath = resolve(ensureString(packagePath));
+	const testsPath = resolve(packagePath, "test"), modulesTestsData = [], indexTestsData = [];
+	const testMappings = await Promise.all(
+		(await resolveModulePaths(packagePath)).map(async filename => {
+			let testFilenames = null;
+			const stats = await lstat(resolve(testsPath, filename), { loose: true });
+			if (stats) {
+				if (stats.isFile()) testFilenames = [filename];
+			} else {
+				const testDirname = filename.slice(0, -3);
+				const testDirnameFilenames = await readdir(resolve(testsPath, testDirname), {
+					depth: Infinity,
+					type: { file: true },
+					pattern: /\.js$/u,
+					ignoreRules: ["git"],
+					loose: true,
+					dirFilter: subdirPath => basename(subdirPath).startsWith("_")
+				});
+				if (testDirnameFilenames && testDirnameFilenames.length) {
+					testFilenames = testDirnameFilenames.map(testDirnameFilename =>
+						join(testDirname, testDirnameFilename)
+					);
+				}
+			}
+			return { filename, testFilenames };
+		})
 	);
-	for (const { modulePath, stats } of modulesData) {
-		if (stats && stats.isFile()) {
+	for (const { filename, testFilenames } of testMappings) {
+		if (testFilenames) {
 			modulesTestsData.push(
-				`require(${ JSON.stringify(`./test/${ modulePath.replace(sepRe, "/") }`) });`
+				...testFilenames.map(
+					testFilename =>
+						`require(${
+							JSON.stringify(`./test/${ testFilename.replace(sepRe, "/") }`)
+						});`
+				)
 			);
 			indexTestsData.push(
-				`\tt.pass(${ JSON.stringify(`Tests found for '${ modulePath }'`) });`
+				`\tt.pass(${ JSON.stringify(`Tests found for '${ filename }'`) });`
 			);
 		} else {
 			indexTestsData.push(
-				`\tt.fail(${ JSON.stringify(`Tests missing for '${ modulePath }'`) });`
+				`\tt.fail(${ JSON.stringify(`Tests missing for '${ filename }'`) });`
 			);
 		}
 	}
@@ -48,5 +73,5 @@ module.exports = async function (path) {
 	push.apply(srcCode, modulesTestsData);
 	srcCode.push("");
 
-	return writeFile(resolve(path, `${ testIndexName }.js`), srcCode.join("\n"));
+	return writeFile(resolve(packagePath, `${ testIndexName }.js`), srcCode.join("\n"));
 };
